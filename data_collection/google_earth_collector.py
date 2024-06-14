d1321807357e42f9e5a293d4042d479ee96b96b2
QinPR <119010249@link.cuhk.edu.cn>
2024-06-14 12:49:02 -0500

    Code Add: Implement large-scale collecting

--- data_collection/google_earth_collector.py
+++ data_collection/google_earth_collector.py
@@ -1,6 +1,13 @@
-import pyautogui
 import os
+import pyautogui
+import pandas as pd
 import time
+
+# ----------------------------------------------- Path Configurations --------------------------------------------------------------
+position_dataset = "/Users/peiranqin/Desktop/African-mining/image_dataset/sample_not_mine.xlsx"
+# position_dataset = None   # for demo
+store_dir = "/Users/peiranqin/Desktop/African-mining/image_dataset/not_mine"
+
 
 # --------------------------------------- Settings for automically collect images --------------------------------------------------
 # please configure these according to your computer
@@ -11,11 +18,18 @@ OPERTION_INTERVAL = 0.2     # the time interval between each operation, to garua
 
 
 def open_google_earth():
+    '''
+        Open the google earth application. (You should download it first on your computer)
+    '''
     os.system(r"open /Applications/Google\ Earth\ Pro.app")
     time.sleep(3)   # wait until application is launched.
     print("Google Earth Pro Opened.")
 
+
 def search_coordinates(lat, lon):
+    '''
+        Click the search bar of google eartch and enter the position (latitude, longitude). 
+    '''
     # click the search bar
     # hard code here, ** you need to set the coordinates yourself! Otherwise this script won't work **.
     pyautogui.click(x=search_bar_pos[0], y=search_bar_pos[1])  # the search bar is at this position.
@@ -28,7 +42,11 @@ def search_coordinates(lat, lon):
     pyautogui.press("enter")
     time.sleep(1)  # wait until the search result is ready
 
+
 def save_image(image_path):
+    '''
+        Save the image according to the specific path.
+    '''
     # save image
     pyautogui.click(x=image_save_icon_pos[0], y=image_save_icon_pos[1])   # click the save image icon
     print("Save Figure Icon Clicked. position: {}".format(pyautogui.position()))
@@ -43,17 +61,37 @@ def save_image(image_path):
     pyautogui.press("enter") 
     time.sleep(1)
     pyautogui.press("enter") 
+    time.sleep(6)   # make sure the image has been saved.
     print("Image Saved: {}".format(image_path))
 
-if __name__ == "__main__":
-    sample_latitude = 41.5
-    sample_longitude = 87.6
 
-    store_dir = "/Users/peiranqin/Desktop/African-mining/image_dataset/not_mine"
-    file_name = "{}_{}.png".format(sample_latitude, sample_longitude)
+def collect_one_image(latitude, longitude):
+    '''
+        Given a specifed latitude and longitude:
+        1. Seatch this location on Google Earth Pro.
+        2. Download the iamge.
+    '''
+    file_name = "{}_{}.png".format(latitude, longitude)
 
     image_store_path = os.path.join(store_dir, file_name)
 
     open_google_earth()
-    search_coordinates(sample_latitude, sample_longitude)
+    search_coordinates(latitude, longitude)
     save_image(image_store_path)
+
+
+
+def main():
+
+    if position_dataset is not None:
+        mine_not_mine_positions = pd.read_excel(position_dataset, engine='openpyxl')
+        for _, row in mine_not_mine_positions.iterrows():
+            collect_one_image(row['Latitude'], row['Longitude'])
+    else:
+        sample_latitude = 48.8587611
+        sample_longitude = 2.2934333
+        collect_one_image(sample_latitude, sample_longitude)
+
+
+if __name__ == "__main__":
+    main()
